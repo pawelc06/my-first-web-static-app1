4fa9c87a8ec21060a25d51a4d644deefc975dcc9
Pawel Czop <pawel.czop@gmail.com>
2025-06-30 14:44:35 +0200

    feat: add API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  template: `<div>{{message}}</div>`,
 })
 export class AppComponent {
-  value = 'World';
+  message = '';
+
+  constructor(private http: HttpClient) {
+    this.http.get('/api/message',{responseType:'text'})
+      .subscribe((resp: any) => {
+        console.log('message.response',resp);
+        this.message = resp;
+      });
+  }
 }
